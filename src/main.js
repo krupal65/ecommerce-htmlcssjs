837783cf1634c64a42065ca5b8b5d4f16c124124
krupal65 <krupalbalar65k@gmail.com>
2025-04-02 22:52:31 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import products from "../api/products.json";
+import { homeproductcard } from "../../homeproductcard";
+console.log(products)
+//this homeproductcard function build in oter file and import here 
+ homeproductcard(products) ;
+    
+
